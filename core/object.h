4bee46355a9239bd44ef50a3a552f4ce3b4c5523
LKM <cnming2@qq.com>
2020-11-07 19:38:31 +0800

    feat: add global object base class

---
+++ core/object.h
@@ -0,0 +1,16 @@
+#ifndef CORE_OBJECT_H
+#define CORE_OBJECT_H
+
+#include "global.h"
+
+namespace CVM::RUNTIME
+{
+    class Object
+    {
+        Object()                       = default;
+        virtual ~Object()              = default;
+        virtual std::string toString() = 0;
+    };
+} // namespace CVM::RUNTIME
+
+#endif
