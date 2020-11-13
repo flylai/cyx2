f3d60e680ef8eaef0cb0ac85b508390b1c1ee7fe
LKM <cnming2@qq.com>
2020-11-13 14:53:00 +0800

    feat: add type conversion

--- core/type.hpp
+++ core/type.hpp
@@ -36,6 +36,32 @@ namespace CVM
         {
             return _value.index() == that._value.index();
         }
+        // type conversion.
+        template<typename T>
+        T as()
+        {
+            if (is<T>()) return value<T>();
+            if (std::is_same<T, std::string>::value) return asString();
+            if (std::is_same<T, int>::value) return asInt();
+            if (std::is_same<T, double>::value) return asDouble();
+        }
+
+      private:
+        std::string asString()
+        {
+            if (is<int>()) return std::to_string(value<int>());
+            if (is<double>()) return std::to_string(value<int>());
+        }
+        int asInt()
+        {
+            if (is<double>()) return static_cast<int>(value<double>());
+            // TODO std::string should not reach here
+        }
+        double asDouble()
+        {
+            if (is<int>()) return static_cast<double>(value<int>());
+            // TODO std::string should not reach here
+        }
 
       private:
         std::variant<int, double, std::string> _value;
