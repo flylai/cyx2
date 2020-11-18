a61d05db5a2ffc26042fb790d5a7a2453099abc7
LKM <cnming2@qq.com>
2020-11-18 19:47:08 +0800

    feat: add token

---
+++ compiler/token.hpp
@@ -0,0 +1,154 @@
+#ifndef CVM_TOKEN_HPP
+#define CVM_TOKEN_HPP
+
+#include <ostream>
+#include <string>
+
+namespace COMPILER
+{
+
+    enum Keyword : char
+    {
+        INVALID,  //
+        IF,       // if
+        ELSE,     // else
+        FOR,      // for
+        WHILE,    // while
+        SWITCH,   // switch
+        BREAK,    // break
+        CONTINUE, // continue
+        TRUE,     // true
+        FALSE,    // false
+        DEF,      // def
+        RETURN,   // return
+        IMPORT,   // import
+
+        // Binary opcode
+
+        // Arithmetic
+        ADD,  // + => a + b
+        SUB,  // - => a - b
+        MUL,  // * => a * b
+        DIV,  // / => a / b
+        MOD,  // % => a % b
+        BAND, // & => a & b
+        BXOR, // ^ => a ^ b
+        BOR,  // | => a | b
+
+        // Logic
+        LAND, // && => a && b
+        LOR,  // || => a || b
+
+        // Comparison
+        EQ, // == => a == b
+        NE, // != => a != b
+        LE, // <= => a <= b
+        LT, // < => a < b
+        GE, // >= => a >= b
+        GT, // > => a > b
+
+        // Other
+        ASSIGN, // = => a = b
+
+        // Unary opcode
+
+        SELFADD, // ++ => a++ / ++a
+        SELFSUB, // -- => a-- / --a
+        LNOT,    // ! => !a
+        BNOT,    // ~ => ~a
+
+        // Other
+        LPAREN,    // (
+        RPAREN,    // )
+        LBRACE,    // {
+        RBRACE,    // }
+        LBRACKET,  // [
+        RBRACKET,  // ]
+        SEMICOLON, // ;
+        COLON,     // :
+
+        INTEGER,
+    };
+
+#define STR(X) (#X)
+
+    class Token
+    {
+      public:
+        Token() = default;
+        Token(Keyword keyword, std::string value, int row, int column)
+            : tk(keyword), value(std::move(value)), row(row), column(column){};
+        Keyword tk{ 0 };
+        std::string value;
+        int row{ 0 };
+        int column{ 0 };
+
+      public:
+        std::string keywordName() const
+        {
+            const char *keyword[] = {
+                STR(INVALID),  //
+                STR(IF),       // if
+                STR(ELSE),     // else
+                STR(FOR),      // for
+                STR(WHILE),    // while
+                STR(SWITCH),   // switch
+                STR(BREAK),    // break
+                STR(CONTINUE), // continue
+                STR(TRUE),     // true
+                STR(FALSE),    // false
+                STR(DEF),      // def
+                STR(RETURN),   // return
+                STR(IMPORT),   // import
+
+                // Binary opcode
+
+                // Arithmetic
+                STR(ADD),  // + => a + b
+                STR(SUB),  // - => a - b
+                STR(MUL),  // * => a * b
+                STR(DIV),  // / => a / b
+                STR(MOD),  // % => a % b
+                STR(BAND), // & => a & b
+                STR(BXOR), // ^ => a ^ b
+                STR(BOR),  // | => a | b
+
+                // Logic
+                STR(LAND), // && => a && b
+                STR(LOR),  // || => a || b
+
+                // Comparison
+                STR(EQ), // == => a == b
+                STR(NE), // != => a != b
+                STR(LE), // <= => a <= b
+                STR(LT), // < => a < b
+                STR(GE), // >= => a >= b
+                STR(GT), // > => a > b
+
+                // Other
+                STR(ASSIGN), // = => a = b
+
+                // Unary opcode
+
+                STR(SELFADD), // ++ => a++ / ++a
+                STR(SELFSUB), // -- => a-- / --a
+                STR(LNOT),    // ! => !a
+                STR(BNOT),    // ~ => ~a
+
+                // Other
+                STR(LPAREN),    // (
+                STR(RPAREN),    // )
+                STR(LBRACE),    // {
+                STR(RBRACE),    // }
+                STR(LBRACKET),  // [
+                STR(RBRACKET),  // ]
+                STR(SEMICOLON), // ;
+                STR(COLON),     // :
+                STR(INTEGER),
+            };
+            return keyword[tk];
+        };
+    };
+} // namespace COMPILER
+
+#endif // CVM_TOKEN_HPP
