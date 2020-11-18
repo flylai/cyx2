d7dd99f1bcf2308ffac4052cd038446d27eb4f65
LKM <cnming2@qq.com>
2020-11-18 21:08:24 +0800

    feat: keyword is supported

--- compiler/lexer.cpp
+++ compiler/lexer.cpp
@@ -1,5 +1,25 @@
 #include "lexer.h"
 
+COMPILER::Lexer::Lexer(std::string raw_code) : raw_code(raw_code)
+{
+    current_char = raw_code[0];
+    pos          = 0;
+    keyword      = {
+        { "if", Keyword::IF },             //
+        { "else", Keyword::ELSE },         //
+        { "for", Keyword::FOR },           //
+        { "while", Keyword::WHILE },       //
+        { "switch", Keyword::SWITCH },     //
+        { "break", Keyword::BREAK },       //
+        { "continue", Keyword::CONTINUE }, //
+        { "true", Keyword::TRUE },         //
+        { "false", Keyword::FALSE },       //
+        { "def", Keyword::DEF },           //
+        { "return", Keyword::RETURN },     //
+        { "import", Keyword::IMPORT },     //
+    };
+}
+
 void COMPILER::Lexer::skipBlank()
 {
     while (isspace(current_char))
@@ -22,36 +42,53 @@ void COMPILER::Lexer::advance()
     if (pos + 1 >= raw_code.size())
         current_char = EOF;
     else
+    {
+        if (current_char == '\n')
+        {
+            column = 0;
+            row++;
+        }
+        else
+        {
+            column++;
+        }
         current_char = raw_code[++pos];
+    }
 }
 
 COMPILER::Token COMPILER::Lexer::nextToken()
 {
+    if (isspace(current_char)) skipBlank();
     if (current_char == '+')
     {
         advance();
-        return Token(TK::ADD, "+", 0, 0);
+        return Token(Keyword::ADD, "+", row, column);
     }
     else if (current_char == '-')
     {
         advance();
-        return Token(TK::SUB, "-", 0, 0);
+        return Token(Keyword::SUB, "-", row, column);
     }
     else if (current_char == '*')
     {
         advance();
-        return Token(TK::MUL, "*", 0, 0);
+        return Token(Keyword::MUL, "*", row, column);
     }
     else if (current_char == '/')
     {
         advance();
-        return Token(TK::DIV, "/", 0, 0);
+        return Token(Keyword::DIV, "/", row, column);
     }
     else if (isdigit(current_char))
     {
-        return Token(TK::INTEGER, number(), 0, 0);
+        return Token(Keyword::INTEGER, number(), row, column);
     }
-    return Token(TK::INVALID, "", 0, 0);
+    else if (current_char == '_' || isalpha(current_char))
+    {
+        return identity();
+    }
+
+    return Token(Keyword::INVALID, "", row, column);
 }
 
 char COMPILER::Lexer::currentChar() const
@@ -59,10 +96,35 @@ char COMPILER::Lexer::currentChar() const
     return current_char;
 }
 
-char COMPILER::Lexer::peek() const
+char COMPILER::Lexer::peekNextChar() const
 {
     if (pos + 1 < raw_code.size())
         return raw_code[pos + 1];
     else
         return EOF;
+}
+
+std::pair<int, int> COMPILER::Lexer::position() const
+{
+    return std::pair<int, int>(row, column);
+}
+
+COMPILER::Token COMPILER::Lexer::identity()
+{
+    std::string tk;
+
+    while (isalnum(current_char) || current_char == '_')
+    {
+        tk.push_back(current_char);
+        advance();
+    }
+
+    try
+    {
+        return Token(keyword.at(tk), "", row, column);
+    }
+    catch (const std::out_of_range &ignore)
+    {
+        return Token(Keyword::IDENTITY, tk, row, column);
+    }
 }
--- compiler/lexer.h
+++ compiler/lexer.h
@@ -5,6 +5,7 @@
 
 #include <cctype>
 #include <string>
+#include <unordered_map>
 #include <utility>
 
 namespace COMPILER
@@ -12,23 +13,26 @@ namespace COMPILER
     class Lexer
     {
       public:
-        explicit Lexer(std::string raw_code) : raw_code(raw_code)
-        {
-            current_char = raw_code[0];
-            pos          = 0;
-        };
-
+        explicit Lexer(std::string raw_code);
         void skipBlank();
-        std::string number();
         void advance();
-        Token nextToken();
         char currentChar() const;
-        char peek() const;
+        char peekNextChar() const;
+        std::pair<int, int> position() const;
+        Token nextToken();
+
+      public:
+        std::string number();
+        Token identity();
 
       private:
         std::string raw_code;
+        std::unordered_map<std::string, Keyword> keyword;
         char current_char{ 0 };
         int pos{ 0 };
+        //
+        int row{ 0 };
+        int column{ 0 };
     };
 
 } // namespace COMPILER
