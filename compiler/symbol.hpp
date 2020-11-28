7e5c064bc0e59f2519b27cfd64faaded4d22ae2b
LKM <cnming2@qq.com>
2020-11-28 18:03:37 +0800

    feat: add `SymbolTable` class

---
+++ compiler/symbol.hpp
@@ -0,0 +1,80 @@
+#ifndef CVM_SYMBOL_HPP
+#define CVM_SYMBOL_HPP
+
+#include <any>
+#include <optional>
+#include <string>
+#include <unordered_map>
+
+namespace COMPILER
+{
+    enum SymbolType
+    {
+        INVALID,
+        INT,
+        DOUBLE,
+        STRING,
+    };
+    class Symbol
+    {
+      public:
+        Symbol() = default;
+        template<typename T>
+        Symbol(const std::string &name, T value)
+        {
+            this->name = name;
+            _value     = value;
+            if constexpr (std::is_same<int, T>()) type = SymbolType::INT;
+            if constexpr (std::is_same<double, T>()) type = SymbolType::DOUBLE;
+            if constexpr (std::is_same<std::string, T>()) type = SymbolType::STRING;
+        };
+
+        SymbolType type{ INVALID };
+        std::string name;
+
+        template<typename T>
+        T value()
+        {
+            return std::any_cast<T>(_value);
+        }
+
+      private:
+        std::any _value;
+    };
+
+    //
+    //
+
+    class SymbolTable
+    {
+      public:
+        SymbolTable()
+        {
+            pre = nullptr;
+        };
+        explicit SymbolTable(SymbolTable *pre) : pre(pre), depth(pre->depth + 1){};
+
+        std::pair<int, std::optional<Symbol>> query(const std::string &key)
+        {
+            SymbolTable *tmp = this;
+            while (tmp != nullptr && tmp->table.find(key) == tmp->table.end())
+            {
+                tmp = tmp->pre;
+            }
+            if (tmp->table.find(key) != tmp->table.end()) return { tmp->depth, table[key] };
+            return { -1, {} };
+        };
+
+        void upsert(const std::string &key, const Symbol &symbol)
+        {
+            table[key] = symbol;
+        };
+
+      private:
+        int depth{ 0 };
+        std::unordered_map<std::string, Symbol> table;
+        SymbolTable *pre{ nullptr };
+    };
+} // namespace COMPILER
+
+#endif // CVM_SYMBOL_HPP
