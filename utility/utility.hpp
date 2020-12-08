4f3da91dd0dd55d1e2e54621f04e4e962967b5d7
LKM <cnming2@qq.com>
2020-12-08 14:36:22 +0800

    feat: add `digit2HexStr`

--- utility/utility.hpp
+++ utility/utility.hpp
@@ -1,6 +1,8 @@
 #ifndef CVM_UTILITY_HPP
 #define CVM_UTILITY_HPP
 
+#include <sstream>
+
 template<typename T, typename... U>
 static inline bool inOr(T lhs, U... args)
 {
@@ -11,6 +13,18 @@ template<typename T, typename... U>
 static inline bool inAnd(T lhs, U... args)
 {
     return ((lhs == args) && ...);
+}
+
+template<typename T>
+static inline std::string digit2HexStr(T value)
+{
+    std::string retval;
+
+    std::stringstream ss;
+    ss << std::hex << value;
+
+    retval = ss.str();
+    return retval;
 }
 
 #endif // CVM_UTILITY_HPP
