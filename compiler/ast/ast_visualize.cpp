da1bcd031e1a74b86f507b44027faad3d0f561ea
LKM <cnming2@qq.com>
2020-12-08 15:12:11 +0800

    feat: more stmts are supported(visualizer)

--- compiler/ast/ast_visualize.cpp
+++ compiler/ast/ast_visualize.cpp
@@ -1,6 +1,9 @@
 #include "ast_visualize.h"
 
-#define NODE(PTR) ("node" + std::to_string(PTR->row) + "_" + std::to_string(PTR->column))
+#define NODE(PTR) "node" + digit2HexStr(static_cast<void *>(PTR))
+#define EDGE(FROM, TO) ((FROM) != nullptr && (TO) != nullptr) ? NODE(FROM) + "->" + NODE(TO) + "\n" : ""
+#define VISIT(PTR)                                                                                                     \
+    if ((PTR) != nullptr) PTR->visit(this)
 
 // Expr
 
@@ -13,16 +16,10 @@ void COMPILER::ASTVisualize::visitBinaryExpr(COMPILER::BinaryExpr *ptr)
     if (ptr == nullptr) return;
 
     graph += NODE(ptr) + "[label=\"BinaryExpr#" + ptr->op.keywordName() + "\"]\n";
-    if (ptr->lhs != nullptr)
-    {
-        graph += NODE(ptr) + "->" + NODE(ptr->lhs) + "\n";
-        ptr->lhs->visit(this);
-    }
-    if (ptr->rhs != nullptr)
-    {
-        graph += NODE(ptr) + "->" + NODE(ptr->rhs) + "\n";
-        ptr->rhs->visit(this);
-    }
+    graph += EDGE(ptr, ptr->lhs);
+    graph += EDGE(ptr, ptr->rhs);
+    VISIT(ptr->lhs);
+    VISIT(ptr->rhs);
 }
 
 void COMPILER::ASTVisualize::visitIntExpr(COMPILER::IntExpr *ptr)
@@ -47,11 +44,11 @@ void COMPILER::ASTVisualize::visitAssignExpr(COMPILER::AssignExpr *ptr)
 {
     if (ptr == nullptr) return;
 
-    graph += NODE(ptr) + "[label=\"BinaryExpr#" + ptr->op.keywordName() + "\"]\n";
-    graph += NODE(ptr) + "->" + NODE(ptr->lhs) + "\n";
-    ptr->lhs->visit(this);
-    graph += NODE(ptr) + "->" + NODE(ptr->rhs) + "\n";
-    ptr->rhs->visit(this);
+    graph += NODE(ptr) + "[label=\"AssignExpr#" + ptr->op.keywordName() + "\"]\n";
+    graph += EDGE(ptr, ptr->lhs);
+    graph += EDGE(ptr, ptr->rhs);
+    VISIT(ptr->lhs);
+    VISIT(ptr->rhs);
 }
 
 void COMPILER::ASTVisualize::visitIdentifierExpr(COMPILER::IdentifierExpr *ptr)
@@ -64,29 +61,70 @@ void COMPILER::ASTVisualize::visitIdentifierExpr(COMPILER::IdentifierExpr *ptr)
 
 void COMPILER::ASTVisualize::visitIfStmt(COMPILER::IfStmt *ptr)
 {
+    if (ptr == nullptr) return;
+    graph += NODE(ptr) + "[label=\"IfStmt#\"]\n";
+    graph += EDGE(ptr, ptr->cond);
+    graph += EDGE(ptr, ptr->true_block);
+    graph += EDGE(ptr, ptr->false_block);
+    VISIT(ptr->cond);
+    VISIT(ptr->true_block);
+    VISIT(ptr->false_block);
 }
 
 void COMPILER::ASTVisualize::visitForStmt(COMPILER::ForStmt *ptr)
 {
+    if (ptr == nullptr) return;
+    graph += NODE(ptr) + "[label=\"ForStmt#\"]\n";
+    graph += EDGE(ptr, ptr->init);
+    graph += EDGE(ptr, ptr->cond);
+    graph += EDGE(ptr, ptr->final);
+    graph += EDGE(ptr, ptr->block);
+
+    VISIT(ptr->init);
+    VISIT(ptr->cond);
+    VISIT(ptr->final);
+    VISIT(ptr->block);
 }
 
 void COMPILER::ASTVisualize::visitWhileStmt(COMPILER::WhileStmt *ptr)
 {
+    if (ptr == nullptr) return;
+    graph += NODE(ptr) + "[label=\"WhileStmt#\"]\n";
+    graph += EDGE(ptr, ptr->cond);
+    graph += EDGE(ptr, ptr->block);
+    VISIT(ptr->cond);
+    VISIT(ptr->block);
 }
 
 void COMPILER::ASTVisualize::visitSwitchStmt(COMPILER::SwitchStmt *ptr)
 {
+    if (ptr == nullptr) return;
+    graph += NODE(ptr) + "[label=\"SwitchStmt#\"]\n";
+    graph += EDGE(ptr, ptr->cond);
+    VISIT(ptr->cond);
+    for (auto x : ptr->matches)
+    {
+        graph += EDGE(ptr, x);
+        VISIT(x);
+    }
 }
 
 void COMPILER::ASTVisualize::visitMatchStmt(COMPILER::MatchStmt *ptr)
 {
+    if (ptr == nullptr) return;
+    graph += NODE(ptr) + "[label=\"MatchStmt#\"]\n";
+    graph += EDGE(ptr, ptr->cond);
+    graph += EDGE(ptr, ptr->block);
+    VISIT(ptr->cond);
+    VISIT(ptr->block);
 }
 
 void COMPILER::ASTVisualize::visitFuncDeclStmt(COMPILER::FuncDeclStmt *ptr)
 {
-    graph += NODE(ptr) + "[label=\"FuncDeclStmt#" + "\"]\n";
-    graph += NODE(ptr) + "->" + NODE(ptr->block) + "\n";
-    visitBlockStmt(ptr->block);
+    if (ptr == nullptr) return;
+    graph += NODE(ptr) + "[label=\"FuncDeclStmt#" + ptr->func_name->value + "\"]\n";
+    graph += EDGE(ptr, ptr->block);
+    VISIT(ptr->block);
 }
 
 void COMPILER::ASTVisualize::visitBreakStmt(COMPILER::BreakStmt *ptr)
@@ -103,23 +141,39 @@ void COMPILER::ASTVisualize::visitReturnStmt(COMPILER::ReturnStmt *ptr)
 
 void COMPILER::ASTVisualize::visitImportStmt(COMPILER::ImportStmt *ptr)
 {
+    if (ptr == nullptr) return;
+    graph += NODE(ptr) + "[label=\"ImportStmt#\\n" + ptr->path + "\"]\n";
 }
 
 void COMPILER::ASTVisualize::visitBlockStmt(COMPILER::BlockStmt *ptr)
 {
-    graph += NODE(ptr) + "[label=\"BlockStmt#" + "\"]\n";
+    if (ptr == nullptr) return;
+    graph += NODE(ptr) + "[label=\"BlockStmt#\"]\n";
 
-    for (auto &x : ptr->stmts)
+    for (auto x : ptr->stmts)
     {
-        graph += NODE(ptr) + "->" + NODE(x) + "\n";
-        x->visit(this);
+        graph += EDGE(ptr, x);
+        VISIT(x);
     }
 }
 
 void COMPILER::ASTVisualize::visitExprStmt(COMPILER::ExprStmt *ptr)
 {
+    if (ptr == nullptr) return;
+    graph += NODE(ptr) + "[label=\"ExprStmt#\"]\n";
+    graph += EDGE(ptr, ptr->expr);
+    VISIT(ptr->expr);
 }
 
 void COMPILER::ASTVisualize::visitTree(COMPILER::Tree *ptr)
 {
+    if (ptr == nullptr) return;
+
+    graph += NODE(ptr) + "[label=\"Tree#\"]\n";
+
+    for (auto x : ptr->stmts)
+    {
+        graph += EDGE(ptr, x);
+        VISIT(x);
+    }
 }
