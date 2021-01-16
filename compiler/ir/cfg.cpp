2c73fb8c9356d8d28167054af85133694c97c6b3
LKM <cnming2@qq.com>
2021-01-16 12:26:05 +0800

    feat: add cfg(Lengauer-Tarjan algorithm)

---
+++ compiler/ir/cfg.cpp
@@ -0,0 +1,90 @@
+#include "cfg.h"
+
+void COMPILER::CFG::init()
+{
+    for (auto block : basic_blocks)
+    {
+        sdom[block]             = block;
+        disjoint_set[block]     = block;
+        disjoint_set_val[block] = block;
+    }
+}
+
+void COMPILER::CFG::dfs(COMPILER::BasicBlock *cur_basic_block)
+{
+    if (cur_basic_block == nullptr) return;
+    if (visited.find(cur_basic_block) != visited.end()) return;
+    visited[cur_basic_block] = true;
+    dfn.push_back(cur_basic_block);
+    dfn_map[cur_basic_block] = dfn.size() - 1;
+    for (auto *block : cur_basic_block->succs())
+    {
+        dfs(block);
+        father[block] = cur_basic_block;
+    }
+}
+
+COMPILER::BasicBlock *COMPILER::CFG::find(COMPILER::BasicBlock *block)
+{
+    if (block == disjoint_set[block]) return block;
+    auto *tmp = find(disjoint_set[block]);
+    if (dfn_map[sdom[disjoint_set_val[disjoint_set[block]]]] < dfn_map[sdom[disjoint_set_val[block]]])
+        disjoint_set_val[block] = disjoint_set_val[disjoint_set[block]];
+    disjoint_set[block] = tmp;
+    return tmp;
+}
+
+void COMPILER::CFG::tarjan()
+{
+    for (unsigned long i = dfn.size() - 1; i >= 1; i--)
+    {
+        auto *cur_block = dfn[i];
+        if (cur_block == nullptr) continue;
+        for (auto *pre_block : cur_block->pres())
+        {
+            if (dfn_map.find(pre_block) != dfn_map.end())
+            {
+                find(cur_block);
+                int a           = dfn_map[sdom[cur_block]];
+                int b           = dfn_map[sdom[disjoint_set_val[pre_block]]];
+                sdom[cur_block] = dfn[std::min(a, b)];
+            }
+        }
+        tree[sdom[cur_block]].insert(cur_block);
+        disjoint_set[cur_block] = father[cur_block];
+        auto *tmp               = disjoint_set[cur_block];
+        for (auto *block : tree[tmp])
+        {
+            find(block);
+            if (dfn_map[sdom[disjoint_set_val[block]]] < dfn_map[father[cur_block]])
+                idom[block] = disjoint_set_val[block];
+            else
+                idom[block] = father[cur_block];
+        }
+        tree[tmp].clear();
+    }
+    for (int i = 1; i < dfn.size(); i++)
+    {
+        auto *cur_block = dfn[i];
+        if (idom[cur_block] != sdom[cur_block]) idom[cur_block] = idom[idom[cur_block]];
+        tree[idom[cur_block]].insert(cur_block);
+    }
+}
+
+void COMPILER::CFG::buildDominateTree()
+{
+    init();
+    dfs(entry);
+    tarjan();
+}
+
+void COMPILER::CFG::showIDom()
+{
+    std::string str;
+    for (const auto &x : idom)
+    {
+        str += (x.first != nullptr ? x.first->name : "null") + " dominated by " +
+               (x.second != nullptr ? x.second->name : "null") + "\n";
+    }
+    log(str);
+}
---
+++ compiler/ir/cfg.h
@@ -0,0 +1,44 @@
+#ifndef CVM_CFG_H
+#define CVM_CFG_H
+
+#include "basicblock.hpp"
+
+#include <unordered_map>
+#include <unordered_set>
+
+namespace COMPILER
+{
+    class CFG
+    {
+
+      public:
+        void buildDominateTree();
+        void showIDom();
+
+      public:
+        std::vector<BasicBlock *> basic_blocks;
+        BasicBlock *entry{};
+
+      private:
+        void init();
+        void dfs(BasicBlock *cur_basic_block);
+        void tarjan();
+        COMPILER::BasicBlock *find(COMPILER::BasicBlock *block);
+
+      private:
+        std::vector<BasicBlock *> dfn;
+        std::unordered_map<BasicBlock *, int> dfn_map;
+        std::unordered_map<BasicBlock *, BasicBlock *> father;
+        //
+        std::unordered_map<BasicBlock *, BasicBlock *> sdom;
+        std::unordered_map<BasicBlock *, BasicBlock *> idom; // the closest point of dominated point
+        std::unordered_map<BasicBlock *, std::unordered_set<BasicBlock *>> tree;
+
+        std::unordered_map<BasicBlock *, BasicBlock *> disjoint_set;
+        std::unordered_map<BasicBlock *, BasicBlock *> disjoint_set_val;
+        //
+        std::unordered_map<BasicBlock *, bool> visited;
+    };
+} // namespace COMPILER
+
+#endif // CVM_CFG_H
