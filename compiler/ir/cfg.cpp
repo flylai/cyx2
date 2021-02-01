22693bfb1b672d678f42627f25a03376bd68bf08
LKM <cnming2@qq.com>
2021-02-01 17:08:29 +0800

    feat: add SSA construction related

--- compiler/ir/cfg.cpp
+++ compiler/ir/cfg.cpp
@@ -2,7 +2,7 @@
 
 void COMPILER::CFG::init()
 {
-    for (auto block : basic_blocks)
+    for (auto block : funcs[0]->blocks)
     {
         sdom[block]             = block;
         disjoint_set[block]     = block;
@@ -17,7 +17,7 @@ void COMPILER::CFG::dfs(COMPILER::BasicBlock *cur_basic_block)
     visited[cur_basic_block] = true;
     dfn.push_back(cur_basic_block);
     dfn_map[cur_basic_block] = dfn.size() - 1;
-    for (auto *block : cur_basic_block->succs())
+    for (auto *block : cur_basic_block->succs)
     {
         dfs(block);
         father[block] = cur_basic_block;
@@ -40,7 +40,7 @@ void COMPILER::CFG::tarjan()
     {
         auto *cur_block = dfn[i];
         if (cur_block == nullptr) continue;
-        for (auto *pre_block : cur_block->pres())
+        for (auto *pre_block : cur_block->pres)
         {
             if (dfn_map.find(pre_block) != dfn_map.end())
             {
@@ -79,7 +79,214 @@ void COMPILER::CFG::buildDominateTree()
     calcDominanceFrontier();
 }
 
-std::string COMPILER::CFG::iDomDetailStr()
+void COMPILER::CFG::calcDominanceFrontier()
+{
+    for (auto *func : funcs)
+    {
+        for (auto *block : func->blocks)
+        {
+            if (block->pres.size() >= 2)
+            {
+                for (auto *pre : block->pres)
+                {
+                    auto *runner = pre;
+                    while (runner != idom[block] && runner != nullptr)
+                    {
+                        dominance_frontier[runner].insert(block);
+                        runner = idom[runner];
+                    }
+                }
+            }
+        }
+    }
+}
+
+void COMPILER::CFG::simplifyCFG()
+{
+    for (auto *func : funcs)
+    {
+        for (auto it = func->blocks.begin(); it != func->blocks.end();)
+        {
+            auto *block = *it;
+            if (block->insts.empty() && !block->pres.empty() && block->succs.size() == 1)
+            {
+                // remove it!
+                auto *succ = *block->succs.begin();
+                succ->pres.erase(block);
+                for (auto *pre : block->pres)
+                {
+                    // remove block from pred's succs
+                    pre->succs.erase(block);
+                    pre->succs.insert(succ);
+                    succ->pres.insert(pre);
+                    // remove block from pred's insts
+                    for (auto *inst : pre->insts)
+                    {
+                        if (inOr(inst->tag, IR::Tag::BRANCH, IR::Tag::JMP))
+                        {
+                            if (inst->tag == IR::Tag::BRANCH)
+                            {
+                                auto *tmp = static_cast<IRBranch *>(inst);
+                                if (tmp->true_block == block) tmp->true_block = succ;
+                                if (tmp->false_block == block) tmp->false_block = succ;
+                            }
+                            else
+                            {
+                                auto *tmp = static_cast<IRJump *>(inst);
+                                if (tmp->target == block) tmp->target = succ;
+                            }
+                        }
+                    }
+                }
+                it = func->blocks.erase(it);
+                delete block;
+            }
+            else
+            {
+                it++;
+            }
+        }
+    }
+}
+
+void COMPILER::CFG::collectVarAssign()
+{
+    for (auto *func : funcs)
+    {
+        var_block_map.clear();
+        for (auto *block : func->blocks)
+        {
+            for (auto *inst : block->insts)
+            {
+                if (inst->tag == IR::Tag::ASSIGN)
+                {
+                    auto *assign = static_cast<IRAssign *>(inst);
+                    if (assign->dest->def == nullptr && assign->dest->is_ir_gen) continue;
+                    var_block_map[assign->dest->name].insert(block);
+                }
+            }
+        }
+    }
+}
+
+void COMPILER::CFG::insertPhiNode()
+{
+    std::queue<BasicBlock *> work_list;
+    std::unordered_map<BasicBlock *, std::string> inserted;
+
+    for (const auto &p : var_block_map)
+    {
+        const std::string var_name = p.first;
+        for (auto *block : p.second)
+        {
+            work_list.push(block);
+        }
+        while (!work_list.empty())
+        {
+            auto *block = work_list.front();
+            work_list.pop();
+
+            for (auto *df_block : dominance_frontier[block])
+            {
+                if (inserted[df_block] != var_name)
+                {
+                    // add phi node
+                    inserted[df_block] = var_name;
+                    auto *phi          = new IRPhi;
+                    phi->name          = var_name;
+                    df_block->phis.push_back(phi);
+                    // add block's dominance frontier to worklist
+                    work_list.push(df_block);
+                }
+            }
+        }
+    }
+}
+
+void COMPILER::CFG::tryRename()
+{
+    for (const auto &p : var_block_map)
+    {
+        counter[p.first] = 0;
+    }
+    rename(entry);
+}
+
+void COMPILER::CFG::rename(COMPILER::BasicBlock *block)
+{
+    for (auto *phi : block->phis)
+    {
+        phi->ssa_index = newId(phi->name);
+    }
+    for (auto *inst : block->insts)
+    {
+        if (inst->tag == IR::Tag::ASSIGN)
+        {
+            auto *assign = static_cast<IRAssign *>(inst);
+            if (assign->src->tag == IR::Tag::BINARY)
+            {
+                auto *src = static_cast<IRBinary *>(assign->src);
+                if (src->lhs != nullptr && src->lhs->tag == IR::Tag::VAR)
+                {
+                    auto *src_lhs      = static_cast<IRVar *>(src->lhs);
+                    src_lhs->ssa_index = getId(src_lhs->name);
+                }
+                if (src->rhs != nullptr && src->rhs->tag == IR::Tag::VAR)
+                {
+                    auto *src_rhs      = static_cast<IRVar *>(src->rhs);
+                    src_rhs->ssa_index = getId(src_rhs->name);
+                }
+            }
+            else if (assign->src->tag == IR::Tag::VAR)
+            {
+                auto *src      = static_cast<IRVar *>(assign->src);
+                src->ssa_index = getId(src->name);
+            }
+            if (assign->dest->def == nullptr && assign->dest->is_ir_gen) continue;
+            assign->dest->ssa_index = newId(assign->dest->name);
+        }
+    }
+    for (auto *succ : block->succs)
+    {
+        for (auto *phi : succ->phis)
+        {
+            phi->args.push_back(phi->name + std::to_string(getId(phi->name)));
+        }
+    }
+    for (auto *succ : tree[block])
+    {
+        rename(succ);
+    }
+    for (auto *inst : block->insts)
+    {
+        if (inst->tag == IR::Tag::ASSIGN)
+        {
+            auto *assign = static_cast<IRAssign *>(inst);
+            if (!stack[assign->dest->name].empty()) stack[assign->dest->name].pop();
+        }
+    }
+    for (auto *phi : block->phis)
+    {
+        if (!stack[phi->name].empty()) stack[phi->name].pop();
+    }
+}
+
+int COMPILER::CFG::newId(const std::string &name)
+{
+    int i = counter[name]++;
+    stack[name].push(i);
+    return i;
+}
+
+int COMPILER::CFG::getId(const std::string &name)
+{
+    if (stack[name].empty()) return 0;
+    return stack[name].top();
+}
+
+/////////////////////////dump as str//////////////////////
+
+std::string COMPILER::CFG::iDomDetailStr() const
 {
     std::string str;
     for (const auto &x : idom)
@@ -90,21 +297,40 @@ std::string COMPILER::CFG::iDomDetailStr()
     return str;
 }
 
-void COMPILER::CFG::calcDominanceFrontier()
+std::string COMPILER::CFG::dominanceFrontierStr() const
 {
-    for (auto *block : basic_blocks)
+    std::string str;
+    for (const auto &p : dominance_frontier)
     {
-        if (block->pres().size() >= 2)
+        str += p.first->name + " : {";
+        for (auto *block : p.second)
         {
-            for (auto *pre : block->pres())
+            str += block->name + " ";
+        }
+        str += "}\n";
+    }
+    return str;
+}
+
+std::string COMPILER::CFG::dumpCFG() const
+{
+    std::string str;
+    for (auto *func : funcs)
+    {
+        for (auto *block : func->blocks)
+        {
+            str += block->name + " [shape=record, label=\"{" + block->name;
+            for (auto *inst : block->insts)
             {
-                auto *runner = pre;
-                while (runner != idom[block])
-                {
-                    dominance_frontier[runner].insert(block);
-                    runner = idom[runner];
-                }
+                str += "|" + inst->toString();
+            }
+            str += "}\"]\n";
+
+            for (auto *succ : block->succs)
+            {
+                str += block->name + " -> " + succ->name + "\n";
             }
         }
     }
+    return str;
 }
--- compiler/ir/cfg.h
+++ compiler/ir/cfg.h
@@ -1,8 +1,13 @@
 #ifndef CVM_CFG_H
 #define CVM_CFG_H
 
+#include "../../utility/utility.hpp"
 #include "basicblock.hpp"
+#include "ir_instruction.hpp"
 
+#include <algorithm>
+#include <queue>
+#include <stack>
 #include <unordered_map>
 #include <unordered_set>
 
@@ -12,11 +17,14 @@ namespace COMPILER
     {
 
       public:
+        void simplifyCFG();
         void buildDominateTree();
-        std::string iDomDetailStr();
+        std::string iDomDetailStr() const;
+        std::string dominanceFrontierStr() const;
+        std::string dumpCFG() const;
 
       public:
-        std::vector<BasicBlock *> basic_blocks;
+        std::vector<IRFunction *> funcs;
         BasicBlock *entry{};
 
       private:
@@ -26,21 +34,32 @@ namespace COMPILER
         void calcDominanceFrontier();
         // disjoint set
         COMPILER::BasicBlock *find(COMPILER::BasicBlock *block);
+        // SSA construction
+        void collectVarAssign();
+        void insertPhiNode();
+        void tryRename();
+        void rename(BasicBlock *block);
+        int newId(const std::string &name);
+        int getId(const std::string &name);
 
       private:
+        // dfs related
         std::vector<BasicBlock *> dfn;
         std::unordered_map<BasicBlock *, int> dfn_map;
         std::unordered_map<BasicBlock *, BasicBlock *> father;
-        //
+        std::unordered_map<BasicBlock *, bool> visited;
+        // dominate tree related
         std::unordered_map<BasicBlock *, BasicBlock *> sdom; // a.k.a semi, not strict.
         std::unordered_map<BasicBlock *, BasicBlock *> idom; // the closest point of dominated point
         std::unordered_map<BasicBlock *, std::unordered_set<BasicBlock *>> tree;
         std::unordered_map<BasicBlock *, std::unordered_set<BasicBlock *>> dominance_frontier;
-        //
+        // disjoint set (union find) related
         std::unordered_map<BasicBlock *, BasicBlock *> disjoint_set;
         std::unordered_map<BasicBlock *, BasicBlock *> disjoint_set_val;
-        //
-        std::unordered_map<BasicBlock *, bool> visited;
+        // SSA construction.....
+        std::unordered_map<std::string, std::unordered_set<BasicBlock *>> var_block_map;
+        std::unordered_map<std::string, int> counter;
+        std::unordered_map<std::string, std::stack<int>> stack;
     };
 } // namespace COMPILER
 
