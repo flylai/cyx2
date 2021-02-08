271521a7f44a7750425b6efe5494d3b5d3b10292
LKM <cnming2@qq.com>
2021-02-09 00:03:34 +0800

    feat: add `as()`

--- utility/utility.hpp
+++ utility/utility.hpp
@@ -1,6 +1,8 @@
 #ifndef CVM_UTILITY_HPP
 #define CVM_UTILITY_HPP
 
+#include "../compiler/ir/ir_instruction.hpp"
+
 #include <sstream>
 
 template<typename T, typename... U>
@@ -33,6 +35,13 @@ static constexpr void addSpace(std::string &str, int n)
     {
         str += " ";
     }
+}
+
+template<typename T, COMPILER::IR::Tag TAG>
+static inline T *as(COMPILER::IR *inst)
+{
+    if (inst->tag == TAG) return static_cast<T *>(inst);
+    return nullptr;
 }
 
 #endif // CVM_UTILITY_HPP
