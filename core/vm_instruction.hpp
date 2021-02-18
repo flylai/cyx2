07307ccc83aecd9d183f347764e093fe56bf4521
LKM <cnming2@qq.com>
2021-02-18 19:53:16 +0800

    refactor: add `toString` and fix some instructions

--- core/vm_instruction.hpp
+++ core/vm_instruction.hpp
@@ -5,12 +5,15 @@
 #include "opcode.hpp"
 
 #include <memory>
+#include <sstream>
+#include <string>
 
 namespace CVM
 {
     struct VMInstruction
     {
         Opcode opcode{ 0xff };
+        virtual std::string toString() = 0;
     };
 
     struct Load : VMInstruction
@@ -24,6 +27,10 @@ namespace CVM
         {
             opcode = Opcode::LOADX;
         }
+        std::string toString() override
+        {
+            return "LOADX %" + std::to_string(reg_idx) + " " + name;
+        }
         std::string name;
     };
 
@@ -33,6 +40,13 @@ namespace CVM
         X()                                                                                                            \
         {                                                                                                              \
             opcode = Opcode::OP;                                                                                       \
+        }                                                                                                              \
+        std::string toString() override                                                                                \
+        {                                                                                                              \
+            std::ostringstream oss;                                                                                    \
+            oss << #OP << " %";                                                                                        \
+            oss << reg_idx << " " << val;                                                                              \
+            return oss.str();                                                                                          \
         }                                                                                                              \
         TYPE val{};                                                                                                    \
     };
@@ -54,6 +68,10 @@ namespace CVM
         {
             opcode = Opcode::STOREX;
         }
+        std::string toString() override
+        {
+            return "STOREX " + name + " %" + std::to_string(reg_idx);
+        }
         int reg_idx{ -1 };
     };
 
@@ -64,6 +82,13 @@ namespace CVM
         {                                                                                                              \
             opcode = Opcode::OP;                                                                                       \
         }                                                                                                              \
+        std::string toString() override                                                                                \
+        {                                                                                                              \
+            std::ostringstream oss;                                                                                    \
+            oss << #OP << " " << name;                                                                                 \
+            oss << " " << val;                                                                                         \
+            return oss.str();                                                                                          \
+        }                                                                                                              \
         TYPE val{};                                                                                                    \
     };
 
@@ -72,12 +97,6 @@ namespace CVM
     STORE_INST(StoreS, STORES, std::string)
 
 #undef STORE_INST
-
-    struct Cmp : VMInstruction
-    {
-        int reg_idx1{ -1 };
-        int reg_idx2{ -1 };
-    };
 
     struct Call : VMInstruction
     {
@@ -89,6 +108,11 @@ namespace CVM
         {
             opcode = Opcode::FUNC;
         }
+        std::string toString() override
+        {
+            return "FUNC " + name + " PARAM COUNT " + std::to_string(param_count);
+        }
+
         std::string name;
         int param_count{ 0 };
     };
@@ -99,22 +123,44 @@ namespace CVM
         {
             opcode = Opcode::PARAM;
         }
+        std::string toString() override
+        {
+            return "PARAM " + name;
+        }
         std::string name;
     };
 
     struct Ret : VMInstruction
     {
+        std::string toString() override
+        {
+            return "RET";
+        }
     };
 
     struct Jmp : VMInstruction
     {
         int target{ -1 };
+        std::string toString() override
+        {
+            return "JMP " + std::to_string(target);
+        }
     };
 
     struct Jif : VMInstruction
     {
         int target1{ -1 };
         int target2{ -1 };
+        std::string toString() override
+        {
+            return "JIF " + std::to_string(target1) + " " + std::to_string(target2);
+        }
+    };
+
+    struct Cmp : VMInstruction
+    {
+        int reg_idx1{ -1 };
+        int reg_idx2{ -1 };
     };
 
 #define CMP_INST(X, OP)                                                                                                \
@@ -124,8 +170,17 @@ namespace CVM
         {                                                                                                              \
             opcode = Opcode::OP;                                                                                       \
         }                                                                                                              \
+        std::string toString() override                                                                                \
+        {                                                                                                              \
+            std::ostringstream oss;                                                                                    \
+            oss << #OP << " %";                                                                                        \
+            oss << reg_idx1 << " %" << reg_idx2;                                                                       \
+            return oss.str();                                                                                          \
+        }                                                                                                              \
     };
 
+    CMP_INST(Lor, LOR)
+    CMP_INST(Land, LAND)
     CMP_INST(Ge, GE)
     CMP_INST(Gt, GT)
     CMP_INST(Le, LE)
@@ -148,6 +203,12 @@ namespace CVM
         {                                                                                                              \
             opcode = Opcode::OP;                                                                                       \
         }                                                                                                              \
+        std::string toString() override                                                                                \
+        {                                                                                                              \
+            std::ostringstream oss;                                                                                    \
+            oss << #OP << " %" << reg_idx1 << " %" << reg_idx2;                                                        \
+            return oss.str();                                                                                          \
+        }                                                                                                              \
     };
 
     ARITHMETIC_INST(Add, ADD)
@@ -161,7 +222,6 @@ namespace CVM
     ARITHMETIC_INST(Bxor, BXOR)
     ARITHMETIC_INST(Shl, SHL)
     ARITHMETIC_INST(Shr, SHR)
-    ARITHMETIC_INST(Lor, LOR)
 
 #undef ARITHMETIC_INST
 
@@ -178,7 +238,6 @@ namespace CVM
         }                                                                                                              \
     };
 
-    UNARY_INST(Land, LAND)
     UNARY_INST(Lnot, LNOT)
     UNARY_INST(Bnot, BNOT)
 
